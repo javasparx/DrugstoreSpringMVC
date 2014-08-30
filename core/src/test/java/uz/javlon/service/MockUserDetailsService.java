c980a560e631e716a4e513f7a1605c1f8e79139d
javasparx <javasparx@gmail.com>
2014-08-30 23:57:31 +0500

    Initial commit

---
+++ core/src/test/java/uz/javlon/service/MockUserDetailsService.java
@@ -0,0 +1,13 @@
+package uz.javlon.service;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import uz.javlon.model.User;
+
+public class MockUserDetailsService implements UserDetailsService {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+        return new User("testuser");
+    }
+}
---
+++ core/src/test/java/uz/javlon/service/impl/PersistentPasswordTokenManagerTest.java
@@ -0,0 +1,25 @@
+package uz.javlon.service.impl;
+
+import org.junit.Ignore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import uz.javlon.service.UserManager;
+
+/**
+ * @author ivangsa
+ */
+@Ignore("create table password_reset_token before running this test")
+public class PersistentPasswordTokenManagerTest extends PasswordTokenManagerTest {
+
+    @Autowired
+    @Qualifier("persistentPasswordTokenManager.userManager")
+    public void setUserManager(UserManager userManager) {
+        super.setUserManager(userManager);
+    }
+
+    @Autowired
+    @Qualifier("persistentPasswordTokenManager")
+    public void setPasswordTokenManager(PasswordTokenManager passwordTokenManager) {
+        super.setPasswordTokenManager(passwordTokenManager);
+    }
+}
